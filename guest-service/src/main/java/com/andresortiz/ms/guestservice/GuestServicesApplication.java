ce6c054a6f04b4ded6931e50ea25abfcb8bdb615
Andres Ortiz <andresortiz248@gmail.com>
2022-10-05 21:05:51 -0500

    add discovery server with eureka

--- guest-service/src/main/java/com/andresortiz/ms/guestservice/GuestServicesApplication.java
+++ guest-service/src/main/java/com/andresortiz/ms/guestservice/GuestServicesApplication.java
@@ -2,8 +2,10 @@ package com.andresortiz.ms.guestservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GuestServicesApplication {
 
 	public static void main(String[] args) {
